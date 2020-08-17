21f48a56dcbd4080ac9f66ea13b6713859273b4f
Pawel <pawelmidur@gmail.com>
2020-08-17 22:05:08 +0200

    Updates

---
+++ precache-manifest.703be68935c15ff9080e6ebaed1efbc1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c26636d55823a5295d4f9c81e02d11f2",
+    "url": "/table-reactjs-and-typescript/index.html"
+  },
+  {
+    "revision": "9826d0db3838376640cc",
+    "url": "/table-reactjs-and-typescript/static/js/2.97fb9b7b.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/table-reactjs-and-typescript/static/js/2.97fb9b7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7c24a49a37a597ad5b7",
+    "url": "/table-reactjs-and-typescript/static/js/main.eea8e60f.chunk.js"
+  },
+  {
+    "revision": "cb14fd408d7c1f5bfdb4",
+    "url": "/table-reactjs-and-typescript/static/js/runtime-main.c4ba9c1a.js"
+  }
+]);
